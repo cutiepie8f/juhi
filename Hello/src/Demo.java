750b9a7941b6253ed5db64703dd1f2a08274c600
dell <dell@192.168.1.7>
2023-07-10 21:50:23 +0530

    Second  commit

---
+++ Hello/src/Demo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Demo {
+	public static void main(String[] args) {
+		int n,fact=1;
+		System.out.println("Enter the number");
+		Scanner scan = new Scanner(System.in);
+		n=scan.nextInt();
+		while(n!=0)
+		{
+			fact=fact*n;
+			n--;
+		}
+		
+System.out.println("The factorial of given number is " +fact);
+System.out.println("hellohdgdhdkfflflmmnws");
+}
+}
